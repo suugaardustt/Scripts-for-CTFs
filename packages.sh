5285ce1a10f74b8c8c7ec5473ca32f7d92a47b73
suugaardustt <samnd2112@gmail.com>
2020-11-04 14:15:58 -0700

    install git / python / clone metasploit added + script concept for nmap install

--- packages.sh
+++ packages.sh
@@ -1,4 +1,20 @@
-## make a new command here later, sudo or not / su or not / input Y/n
+#!/usr/bin/bash
+# make a new command here later, input Y/n for packages (or just keep it automated)
+# make new file for deciding packages, like individual packages or packs of them (like network tools)
+#
 
+apt-get install git
+apt-get install python
 apt-get install net-tools
 apt-get install neofetch
+apt-get install 
+git clone https://github.com/rapid7/metasploit-framework
+
+
+#script in progress for nmap, needs to install from a link on their website and use script below
+#bzip2 -cd nmap-7.91.tar.bz2 | tar xvf -
+#cd nmap-7.91
+#./configure
+#make
+#su root
+#make install
